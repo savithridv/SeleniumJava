d6acea5d52a2f12d25e29c96d03584faf7334091
savithri.dv <savithri.dv@Dell>
2026-09-25 22:31:53 +0530

    commit

---
+++ src/main/java/Selenium/Selenium/App.java
@@ -0,0 +1,15 @@
+package Selenium.Selenium;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+/**
+ * Hello world!
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://www.google.com");
+    }
+}
